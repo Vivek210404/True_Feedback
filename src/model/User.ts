73df863115fd34646f37ec29e12682656bfe1f0f
vivek210404 <vivek210404@gmail.com>
2025-10-27 02:09:25 +0530

    Implemented accept-messages, get-messages and send-message routes

--- src/model/User.ts
+++ src/model/User.ts
@@ -24,7 +24,7 @@ export interface User extends Document {
   verifyCode: string;
   verifyCodeExpiry: Date;
   isVerified: boolean;
-  isAcceptingMessage: boolean;
+  isAcceptingMessages: boolean;
   messages: Message[];
 }
 
@@ -63,7 +63,7 @@ const UserSchema: Schema<User> = new Schema({
     default: false,
   },
 
-  isAcceptingMessage: {
+  isAcceptingMessages: {
     type: Boolean,
     default: false,
   },
